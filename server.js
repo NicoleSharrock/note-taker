f2f36217fcec6d36826b63f57cf8cde8635ce855
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-05-06 18:49:44 -0400

    added function to create a new note

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express')
 const app = express();
-// const allNotes = require('./db/db.json');
+const noteData = require('./db/db.json');
 
 
 app.use(express.json());
@@ -11,22 +11,46 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-
-app.get('/', function (req, res) {
+// route to main page
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 
+// route to notes.html
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-
+// route to save all notes as json
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.json(noteData.slice(1));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
 });
 
 
 
-
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
