bd64a4228dc8917c69d595d924bef9943a222da3
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-05-06 18:31:08 -0400

    added routes to the notes.html, index.html, and to the db.json files

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const express = require('express')
+const PORT = process.env.PORT || 3000;
+const fs = require('fs');
 const path = require('path');
-const app = express()
-const PORT = 3000;
+const express = require('express')
+const app = express();
+// const allNotes = require('./db/db.json');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 
 
 
